acd413448d8f17b581bd5f42b32bb28800c1f4f1
manav13004 <manav65781947@gmail.com>
2026-09-19 16:56:05 +0530

    Create task 1.cpp

---
+++ task 1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    int number, guess;
+
+    // Generate random number
+    srand(time(0));
+    number = rand() % 100 + 1;
+
+    cout << "Guess the number between 1 and 100!" << endl;
+
+    do {
+        cout << "Enter your guess: ";
+        cin >> guess;
+
+        if (guess > number) {
+            cout << "Too high! Try again." << endl;
+        }
+        else if (guess < number) {
+            cout << "Too low! Try again." << endl;
+        }
+        else {
+            cout << "Congratulations! You guessed the correct number." << endl;
+        }
+
+    } while (guess != number);
+
+    return 0;
+}
